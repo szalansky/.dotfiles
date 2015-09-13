f009776a8bccf947db07d9482fea0863ee15f9f6
Raf Szalanski <rafal.szalanski@gmail.com>
2015-09-13 10:10:12 +0000

    Basic install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+
+echo "Cloning szalansky/.dotfiles to $HOME"
+
+git clone git@github.com:szalansky/.dotfiles.git $HOME/.dotfiles
+
+echo "Symlinking .tmux, .gitconf and .vimrc & .vim/colors"
+
+ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+ln -sd $HOME/.dotfiles/.vim/colors/ $HOME/.vim/
+
+echo "Installing Vundle"
+git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+
+
