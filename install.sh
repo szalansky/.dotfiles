59e92577f17dd8ec4a9bc02a890ee4a6622332be
Rafal Szalanski <rafal.szalanski@onfido.com>
2017-08-01 10:19:49 +0100

    gitmessage

--- install.sh
+++ install.sh
@@ -10,8 +10,10 @@ echo "Symlinking .tmux, .gitconf and .vimrc & .vim/colors"
 
 ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/.dotfiles/.gitmessage $HOME/.gitmessage
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -sd $HOME/.dotfiles/.vim/colors/ $HOME/.vim/
+ln -s $HOME/.dotfiles/.profile $HOME/.profile
 
 echo "Installing Vundle"
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
